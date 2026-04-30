bdc440042b95e8689b0b1bcf46d6d5b88d55717d
MiriAvasha <m6741429@gmail.com>
2026-04-30 18:54:02 +0300

    Testing pipeline failure

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 
 function add(a, b) {
-   return a + b + 1;
+    return a + b + 1;
 }
 
 app.get('/', (req, res) => {
-   res.send('CI/CD Pipeline is Working!');
+    res.send('CI/CD Pipeline is Working!');
 });
 
 module.exports = { app, add };
 
 if (require.main === module) {
-   app.listen(3000, () => console.log('Server running on port 3000'));
+    app.listen(3000, () => console.log('Server running on port 3000'));
 }
